741c248c9bfb05c34692756e67378d6d1d54e77b
minju <alswn1583@naver.com>
2023-12-08 15:29:34 +0900

    투두리스트2 추가 기능 완성(삭제 기능 필요)

---
+++ todolist2/src/components/TodoBoard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export default function TodoBoard(props) {
+  return (
+    <>
+      <h1>Todo List</h1>
+      {props.todoList.map((item, idx) => (
+        <TodoItem item={item} key={idx} />
+      ))}
+    </>
+  );
+}
